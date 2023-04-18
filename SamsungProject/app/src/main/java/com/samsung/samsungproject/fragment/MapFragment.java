8f8f60e8ec5f9ab7779d2d7beaeeed6d987ed9e9
KKakavo <pooreshqa2805@gmail.com>
2023-04-19 00:55:55 +0300

    something

--- SamsungProject/app/src/main/java/com/samsung/samsungproject/fragment/MapFragment.java
+++ SamsungProject/app/src/main/java/com/samsung/samsungproject/fragment/MapFragment.java
@@ -3,6 +3,8 @@ package com.samsung.samsungproject.fragment;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.location.Location;
@@ -10,7 +12,12 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
@@ -21,8 +28,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.ResolvableApiException;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.location.SettingsClient;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -32,6 +46,7 @@ import com.google.android.gms.maps.model.JointType;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.samsung.samsungproject.R;
 
@@ -47,12 +62,16 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
     private Location lastLocation;
     private Polyline polyline;
     private List<LatLng> polylinePoints;
-    private Boolean isLocationPermissionGranted;
     private final String LOG_TAG = "TAG";
     private final int LOCATION_PERMISSION_REQUEST = 1;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (!isLocationPermissionGranted()) {
+            launchLocationPermissionRequest();
+        }
+
         polylinePoints = new ArrayList<>();
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireActivity());
         locationManager = (LocationManager) requireActivity().getSystemService(Context.LOCATION_SERVICE);
@@ -62,7 +81,6 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_map, container, false);
-
         SupportMapFragment supportMapFragment = (SupportMapFragment) getChildFragmentManager()
                 .findFragmentById(R.id.fr_google_map);
         supportMapFragment.getMapAsync(this);
@@ -74,92 +92,76 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
         this.googleMap = googleMap;
-        checkLocationPermisson();
-        updateLocationUI();
-        getLastLocation();
-        locationListener = new LocationListener() {
-            @Override
-            public void onLocationChanged(@NonNull Location location) {
-                LatLng currentLocation = new LatLng(location.getLatitude(), location.getLongitude());
-                polylinePoints.add(currentLocation);
-                if(polyline != null){
-                    polyline.setPoints(polylinePoints);
-                } else {
-                    polyline = googleMap.addPolyline(new PolylineOptions().addAll(polylinePoints).color(Color.RED).jointType(JointType.ROUND).width(10.0f));
+        if(isLocationPermissionGranted())
+            enableMyLocation();
+    }
+
+
+    private void launchLocationPermissionRequest() {
+        registerForActivityResult(new ActivityResultContracts.RequestPermission(),
+                result -> {
+                    if (result)
+                        enableMyLocation();
+                    else
+                        shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION);
+                }).launch(Manifest.permission.ACCESS_FINE_LOCATION);
+    }
+
+    private boolean isLocationPermissionGranted() {
+
+        return ContextCompat.checkSelfPermission(requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+
+    }
+
+    private void enableMyLocation() {
+        try {
+            googleMap.setMyLocationEnabled(true);
+            googleMap.getUiSettings().setMyLocationButtonEnabled(true);
+
+        } catch (SecurityException e) {
+            Log.e(LOG_TAG, e.getMessage());
+        }
+    }
+
+    private void enableLocationSettings() {
+        LocationRequest locationRequest = LocationRequest.create()
+                .setInterval(5000)
+                .setFastestInterval(2000)
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(locationRequest);
+
+        SettingsClient client = LocationServices.getSettingsClient(requireActivity());
+        Task<LocationSettingsResponse> task = client.checkLocationSettings(builder.build());
+
+        task.addOnCompleteListener(task1 -> {
+            try {
+                LocationSettingsResponse response = task1.getResult(ApiException.class);
+            } catch (ApiException apiException) {
+
+                switch (apiException.getStatusCode()) {
+
+                    case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                        try {
+                            ResolvableApiException resolvableApiException = (ResolvableApiException) apiException;
+                            resolvableApiException.startResolutionForResult(requireActivity(), 2);
+                        } catch (IntentSender.SendIntentException sendIntentException) {
+                            sendIntentException.printStackTrace();
+                        }
+                        break;
+
+                    case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                        Log.e(LOG_TAG, "Location is now available on this device");
                 }
+
             }
-        };
-        try {
-            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
-        } catch (SecurityException e){
-            Log.e(LOG_TAG, e.getMessage());
-        }
+        });
 
     }
-
-    private void checkLocationPermisson() {
-
-        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION)
-                == PackageManager.PERMISSION_GRANTED
-                && ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION)
-                == PackageManager.PERMISSION_GRANTED)
-            isLocationPermissionGranted = true;
-        else
-            ActivityCompat.requestPermissions(requireActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                    LOCATION_PERMISSION_REQUEST);
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        isLocationPermissionGranted = false;
-        switch (requestCode){
-            case LOCATION_PERMISSION_REQUEST:
-                if(grantResults.length > 0
-                && grantResults[0] == PackageManager.PERMISSION_GRANTED)
-                    isLocationPermissionGranted = true;
-                break;
-            default:
-                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-                break;
-        }
-        updateLocationUI();
-    }
-
-    private void updateLocationUI(){
-        if(googleMap == null)
-            return;
-        try {
-            if (isLocationPermissionGranted) {
-                googleMap.setMyLocationEnabled(true);
-                googleMap.getUiSettings().setMyLocationButtonEnabled(true);
-            } else {
-                googleMap.setMyLocationEnabled(false);
-                googleMap.getUiSettings().setMyLocationButtonEnabled(false);
-                checkLocationPermisson();
-            }
-        } catch (SecurityException e){
-            Log.e(LOG_TAG, e.getMessage());
-        }
-    }
-
-    private void getLastLocation() {
-        try {
-            if (isLocationPermissionGranted) {
-                Task<Location> locationTask = fusedLocationProviderClient.getLastLocation();
-                locationTask.addOnCompleteListener(requireActivity(), task -> {
-                    if (task.isSuccessful()) {
-                        lastLocation = task.getResult();
-                        if (lastLocation != null)
-                            Toast.makeText(getContext(), lastLocation.getLongitude() + " " + lastLocation.getLatitude(), Toast.LENGTH_LONG).show();
-                    }
-                });
-            }
-        } catch (SecurityException e){
-            Log.e(LOG_TAG, e.getMessage());
-        }
-    }
-
 
 
 }
