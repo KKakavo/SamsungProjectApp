cd5c63ac027e7916fc3d19bda052aebba9e8a57c
KKakavo <99440353+KKakavo@users.noreply.github.com>
2023-06-13 20:08:12 +0300

    bug fix
    Wrong color in leaderboard

--- SamsungProject/app/src/main/java/com/samsung/samsungproject/feature/leaderboard/ui/recycler/UserViewHolder.java
+++ SamsungProject/app/src/main/java/com/samsung/samsungproject/feature/leaderboard/ui/recycler/UserViewHolder.java
@@ -21,12 +21,14 @@ import android.content.res.Resources.Theme;
 public class UserViewHolder extends RecyclerView.ViewHolder {
     private final LeaderboardItemBinding binding;
     private User authorizedUser;
+
     public UserViewHolder(LeaderboardItemBinding binding, User authorizedUser) {
         super(binding.getRoot());
         this.binding = binding;
         this.authorizedUser = authorizedUser;
     }
-    public void bind(User user, int position){
+
+    public void bind(User user, int position) {
         Theme theme = binding.getRoot().getContext().getTheme();
         binding.tvPlace.setText(String.valueOf(position + 2));
         binding.tvNickname.setText("@" + user.getNickname());
@@ -34,13 +36,13 @@ public class UserViewHolder extends RecyclerView.ViewHolder {
         TypedValue typedValue = new TypedValue();
         if (user.getEmail().equals(authorizedUser.getEmail())) {
             theme.resolveAttribute(com.google.android.material.R.attr.colorAccent, typedValue, true);
-            if(position>=49)
+            if (position >= 49)
                 binding.tvPlace.setText("");
-        } else{
+        } else {
             theme.resolveAttribute(com.google.android.material.R.attr.colorPrimary, typedValue, true);
         }
         binding.itemBackground.setBackgroundTintList(ContextCompat.getColorStateList(binding.getRoot().getContext(), typedValue.resourceId));
-        switch (position){
+        switch (position) {
             case 0:
                 binding.tvPlace.setTextColor(ContextCompat.getColor(binding.getRoot().getContext(), R.color.gray_BD));
                 binding.tvNickname.setTextColor(ContextCompat.getColor(binding.getRoot().getContext(), R.color.gray_BD));
@@ -48,6 +50,12 @@ public class UserViewHolder extends RecyclerView.ViewHolder {
             case 1:
                 binding.tvPlace.setTextColor(ContextCompat.getColor(binding.getRoot().getContext(), R.color.orange));
                 binding.tvNickname.setTextColor(ContextCompat.getColor(binding.getRoot().getContext(), R.color.orange));
+                break;
+            default:
+                TypedValue color = new TypedValue();
+                theme.resolveAttribute(com.google.android.material.R.attr.colorOnPrimary, color, true);
+                binding.tvPlace.setTextColor(ContextCompat.getColor(binding.getRoot().getContext(), color.resourceId));
+                binding.tvNickname.setTextColor(ContextCompat.getColor(binding.getRoot().getContext(), color.resourceId));
                 break;
         }
     }
